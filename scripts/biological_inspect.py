34fffe9d3318ec8b85eda4fad879617ba94c61e5
Szymon Nowakowski <szymon.nowakowski@gmail.com>
2026-04-15 11:47:03 +0200

    a slim code for inspection

--- scripts/biological_inspect.py
+++ scripts/biological_inspect.py
@@ -72,10 +72,10 @@ def run_bio_inference():
             dummy_tensor = torch.zeros_like(input_tensor)
 
             # Pass both to the model as a list
-            model.set_input([input_tensor, dummy_tensor])
+            model.set_input([dummy_tensor, input_tensor])
 
             model.forward_nograd()
-            translated_tensor = model.images.fake_b
+            translated_tensor = model.images.fake_a
 
         if translated_tensor is not None:
             # --- 3. UNNORMALIZE USING REAL STATS ---
